f9865d6dab244a0d4dda988cd54636b2f5f2d031
unjokerino <soverinLie@gmail.com>
2020-03-18 20:23:08 +0500

    done

--- js/index.js
+++ js/index.js
@@ -66,6 +66,8 @@ vueData = {
     searchTimeOut: false
 };
 
+
+
 var app = new Vue({
     el: "#search_box",
     data: vueData,
@@ -505,3 +507,18 @@ async function searchByCadNumber(cn, type) {
 
     return res
 }
+
+function toggleFullscreen(event) {
+    var element = document.body;
+  
+      if (event instanceof HTMLElement) {
+          element = event;
+      }
+  
+      var isFullscreen = document.webkitIsFullScreen || document.mozFullScreen || false;
+  
+      element.requestFullScreen = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || function () { return false; };
+      document.cancelFullScreen = document.cancelFullScreen || document.webkitCancelFullScreen || document.mozCancelFullScreen || function () { return false; };
+  
+      isFullscreen ? document.cancelFullScreen() : element.requestFullScreen();
+  }
